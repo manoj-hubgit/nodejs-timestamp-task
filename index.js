211735a856d1024b584db44327a38a3d5503aace
manoj-hubgit <manojannadurai2265@gmail.com>
2024-06-06 22:54:30 +0530

    format updated

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get('/',(req,res)=>{
 })
 
 app.get('/current-time',(req,res)=>{
-     let today=format(new Date(),'dd-mm-yyyy-hh-mm-ss')
+     let today=format(new Date(),'dd-mm-yyyy-HH-mm-ss')
      const filepath=`Timestamp/${today}.txt`
      fs.writeFileSync(filepath,`${today}`,'utf8')
      let data=fs.readFileSync(filepath,'utf8')
