5a1d82b4cf9d30c025f22a1a52be145709176217
manoj-hubgit <manojannadurai2265@gmail.com>
2024-06-06 20:36:56 +0530

    files Uploaded

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import fs from "fs";
+import path from "path";
+import { format } from "date-fns";
+import { fileURLToPath } from "url";
+
+const app=express();
+const PORT=4000;
+
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+    res.status(200).send("helloworld")
+})
+
+app.get('/current-time',(req,res)=>{
+     let today=format(new Date(),'dd-mm-yyyy-HH-mm-ss')
+     const filepath=`Timestamp/${today}.text`
+     fs.writeFileSync(filepath,`${today}`,'utf8')
+     let data=fs.readFileSync(filepath,'utf8')
+     res.status(200).send(data)  
+})
+app.get('/all-time',(req,res) => {
+    const __Timestamp =fileURLToPath(import.meta.url)
+   const __dirname=path.dirname(__Timestamp)
+    const all = path.join(__dirname, 'Timestamp');
+    const alldata = fs.readdirSync(all,'utf8');
+    res.status(200).send(alldata);
+    
+});
+
+app.listen(PORT,()=>{
+    console.log("node is listining");
+})
+
